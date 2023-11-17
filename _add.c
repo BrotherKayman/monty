ec591b7f0db2b1ce464f1cee9138686b75e36a33
Kagiso Motlhaoleng <kagisokayman@gmail.com>
2023-11-17 12:06:53 +0200

    Modified

--- _add.c
+++ _add.c
@@ -1,24 +1,64 @@
 #include "main.h"
 
-/**
- * add - Function to add the top two elements of the stack
- * @stack: Pointer to the stack
- * @line_number: Line number in the file
- */
-void _add(stack_t **stack, unsigned int line_number) 
+int is_stack_too_short(stack_t **stack, unsigned int line_number) 
 {
-    stack_t *temp;
-
     if (*stack == NULL || (*stack)->next == NULL) 
     {
-        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+        fprintf(stderr, "L%d: stack too short\n", line_number);
+        return (1);
+    }
+    return (0);
+}
+
+void _swap(stack_t **stack, unsigned int line_number) 
+{
+    if (is_stack_too_short(stack, line_number)) 
+    {
         exit(EXIT_FAILURE);
     }
 
-    temp = (*stack)->next;
-    temp->n += (*stack)->n;
+    int tmp = (*stack)->n;
+    (*stack)->n = (*stack)->next->n;
+    (*stack)->next->n = tmp;
+}
 
-    temp = *stack;
-    *stack = (*stack)->next;
-    free(temp);
+void _add(stack_t **stack, unsigned int line_number) 
+{
+    if (is_stack_too_short(stack, line_number)) 
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    int sum = (*stack)->n + (*stack)->next->n;
+    _pop(stack, line_number);
+    (*stack)->n = sum;
+}
+
+void _nop(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number) 
+{
+}
+
+
+void _pchar(stack_t **stack, unsigned int line_number) 
+{
+    if (*stack == NULL) 
+    {
+        fprintf(stderr, "L%d: stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int val = (*stack)->n;
+    if (val < 0 || val > 127) 
+    {
+        fprintf(stderr, "L%d: value out of range\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    putchar(val);
+    putchar('\n');
+}
+
+int _isalpha(int c) 
+{
+    return (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? 1 : 0);
 }
